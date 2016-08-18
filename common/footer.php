3ec2d7b3d6b0271029dfd2ce0d1cb7bc6ce00f72
hoppaang <hoppaang@gmail.com>
2016-08-18 14:17:41 +0900

    ceo style

--- common/footer.php
+++ common/footer.php
@@ -32,8 +32,6 @@
 	<script type="text/javascript">
 		jQuery(document).ready(function() {
 			App.init();
-			// ContactPage.initMap();
-
 			ContactForm.initContactForm();
 			OwlCarousel.initOwlCarousel();
 			StyleSwitcher.initStyleSwitcher();
