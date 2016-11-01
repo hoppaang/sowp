1ac5adb271080568501afb1084a201cf4baa3deb
hoppaang <hoppaang@gmail.com>
2016-11-01 21:13:29 +0900

    online

--- online/online.php
+++ online/online.php
@@ -14,13 +14,6 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-9 padding-lclear">
-					<!-- Google Map -->
-					<!-- <div id="map" class="map map-box map-box-space margin-bottom-40"></div> -->
-					<div class="map map-box margin-bottom-40">
-						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12920.149804396666!2d126.67338958420723!3d35.946053780572946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3570597634c10ab5%3A0x90c974df65c9417a!2s558+Daehak-ro%2C+Gunsan%2C+Jeollabuk-do!5e0!3m2!1sen!2skr!4v1471499492283" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
-					</div>
-					<!-- End Google Map -->
-
 					<form action="contacts-process.php" method="post" id="sky-form3" class="sky-form contact-style">
 						<fieldset class="no-padding">
 							<label>Name <span class="color-red">*</span></label>
@@ -80,14 +73,7 @@
 
 					<!-- Why we are? -->
 					<!--
-<div class="headline"><h2>Why we are?</h2></div>
-					<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum.</p>
-					<ul class="list-unstyled">
-						<li><i class="fa fa-check color-green"></i> Odio dignissimos ducimus</li>
-						<li><i class="fa fa-check color-green"></i> Blanditiis praesentium volup</li>
-						<li><i class="fa fa-check color-green"></i> Eos et accusamus</li>
-					</ul>
- -->
+
 				</div><!--/col-md-3-->
 		</div><!--/row-->
 	</div>
