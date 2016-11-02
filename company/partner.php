40e823a8d294fc080e357c3f08d5d69dc78e42d6
hoppaang <hoppaang@gmail.com>
2016-11-02 14:47:01 +0900

    partner

--- company/partner.php
+++ company/partner.php
@@ -15,7 +15,7 @@
 			<div class="headline"><a href="<?=$baseurl?>company/partner.php"><h2>참여기업</h2></a></div>
 			<div class="partnet-item">
 				<img src="<?=$imageurl?>img-partner1.png"/>
-				<p><i class="fa fa-phone"></i> 1588-3322</p>
+				<p><i class="fa fa-phone"></i> 1588.3322</p>
 				<a href="http://www.kdbdw.com" target="_blank">www.kdbdw.com</a>
 			</div>
 			<div class="partnet-item">
@@ -25,131 +25,75 @@
 			</div>
 			<div class="partnet-item">
 				<img src="<?=$imageurl?>img-partner4.png"/>
-				<p>063.467.4836</p>
+				<p><i class="fa fa-phone"></i> 063.467.4836</p>
 				<a href="#" target="_blank">#</a>
 			</div>
 			<div class="partnet-item">
 				<img src="<?=$imageurl?>img-partner6.png"/>
-				<p>031.704.1700 </p>
+				<p><i class="fa fa-phone"></i> 031.704.1700 </p>
 				<a href="http://www.hanildevelop.co.kr" target="_blank">www.hanildevelop.co.kr</a>
 			</div>
 			<div class="partnet-item">
 				<img src="<?=$imageurl?>img-partner16.png"/>
-				<p>1.817.281.0040 (U.S.A)</p>
+				<p><i class="fa fa-phone"></i> 1.817.281.0040 (U.S.A)</p>
 				<a href="http://orangeenergycorp.com/" target="_blank">orangeenergycorp.com</a>
 			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="headline"><a href="<?=$baseurl?>company/partner.php"><h2>협력기관</h2></a></div>
+			<div class="partnet-item">
+				<img src="<?=$imageurl?>img-partner15.png"/>
+				<p><i class="fa fa-phone"></i> 063.469.7553~74</p>
+				<a href="http://hp.kunsan.ac.kr/sanhak/index.kunsan" target="_blank">hp.kunsan.ac.kr/sanhak/index.kunsan</a>
+			</div>
+			<div class="partnet-item">
+				<img src="<?=$imageurl?>img-partner13.png"/>
+				<p><i class="fa fa-phone"></i> 063.219.2114</p>
+				<a href="http://www.jbtp.or.kr" target="_blank">www.jbtp.or.kr</a>
+			</div>
+			<div class="partnet-item">
+				<img src="<?=$imageurl?>img-partner8.png"/>
+				<p><i class="fa fa-phone"></i> 02.553.6426</p>
+				<a href="http://www.kweia.or.kr" target="_blank">www.kweia.or.kr</a>
+			</div>
+			<div class="partnet-item">
+				<img src="<?=$imageurl?>img-partner14.png"/>
+				<p><i class="fa fa-phone"></i> 063.219.3500</p>
+				<a href="http://www.kctech.re.kr" target="_blank">kctech.re.kr</a>
+			</div>
+			<div class="partnet-item">
+				<img src="<?=$imageurl?>img-partner9.png"/>
+				<p><i class="fa fa-phone"></i> </p>
+				<!-- <a href="" target="_blank"></a> -->
+			</div>
 		</div>
 		<div class="col-md-4">
 			<div class="headline"><a href="<?=$baseurl?>company/partner.php"><h2>관련기관</h2></a></div>
-		</div>
-	</div>
-	<div class="subcontent">
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner1.png"/>
-			<label>KDB대우증권</label>
-			<p>글로벌 금융투자 은행</p>
-			<a href="http://www.kdbdw.com" target="_blank">www.kdbdw.com</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner2.png"/>
-			<label>한전KPS</label>
-			<p>초인류 전력설비 정비전문회사</p>
-			<a href="http://www.kps.co.kr/" target="_blank">www.kps.co.kr</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner3.png"/>
-			<label>현대유엔아이</label>
-			<p>IT 서비스 전문업체,물류컨설팅,RFID 솔루션</p>
-			<a href="http://www.hyundai-uni.com" target="_blank">www.hyundai-uni.com</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner4.png"/>
-			<label>SMDE</label>
-			<p>새만금 해양복합단지 시행 특수목적법인</p>
-			<a href="#" target="_blank">#</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner5.png"/>
-			<label>(주)통해</label>
-			<p>전기공사,정보통신공사,소방공사,신재생에너지</p>
-			<a href="http://www.thgec.com/" target="_blank">www.thgec.com</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner6.png"/>
-			<label>한일개발</label>
-			<p>건설 전문업체, 건축, 토목, 조경 사업 등</p>
-			<a href="http://www.hanildevelop.co.kr" target="_blank">www.hanildevelop.co.kr</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner7.png"/>
-			<label>스키너스</label>
-			<p>건물 및 토목엔지니어링 서비스업</p>
-			<!-- <a href="" target="_blank"></a> -->
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner8.png"/>
-			<label>한국풍력산업협회</label>
-			<p>한국풍력기술개발, 발전현황 및 통계 제공</p>
-			<a href="http://www.kweia.or.kr" target="_blank">www.kweia.or.kr</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner9.png"/>
-			<label>새만금조선해양레저협회</label>
-			<p>해양레저 클러스터</p>
-			<!-- <a href="" target="_blank"></a> -->
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner10.png"/>
-			<label>산업통상자원부</label>
-			<p>무역,산업,기술,에너지 등 정책/동향 안내</p>
-			<a href="http://www.motie.go.kr" target="_blank">www.motie.go.kr</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner11.png"/>
-			<label>전라북도청</label>
-			<p>전라북도청 도정정보, 여권발급, 행정민원 등</p>
-			<a href="http://www.jeonbuk.go.kr" target="_blank">www.jeonbuk.go.kr</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner12.png"/>
-			<label>새만금개발청</label>
-			<p>새만금사업 총괄 국가기관</p>
-			<a href="http://www.saemangeum.go.kr" target="_blank">www.saemangeum.go.kr</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner13.png"/>
-			<label>전북테크노파크</label>
-			<p>지식기반산업 창업지원, 사업안내</p>
-			<a href="http://www.jbtp.or.kr" target="_blank">www.jbtp.or.kr</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner14.png"/>
-			<label>한국탄소융합기술원</label>
-			<p>탄소복합재/나노튜브분야 등 연구</p>
-			<a href="http://wwwkctech.re.kr" target="_blank">kctech.re.kr</a>
-		</div>
-
-		<div class="partnet-item">
-			<img src="<?=$imageurl?>img-partner15.png"/>
-			<label>군산대학교산학협력단</label>
-			<p>사업체 및 유관기관과 공동연구 기관</p>
-			<a href="http://hp.kunsan.ac.kr/sanhak/index.kunsan" target="_blank">hp.kunsan.ac.kr/sanhak/index.kunsan</a>
+			<div class="partnet-item">
+				<img src="<?=$imageurl?>img-partner10.png"/>
+				<p><i class="fa fa-phone"></i> 1577.0900</p>
+				<a href="http://www.motie.go.kr" target="_blank">www.motie.go.kr</a>
+			</div>
+			<div class="partnet-item">
+				<img src="<?=$imageurl?>img-partner17.png"/>
+				<p><i class="fa fa-phone"></i> 110</p>
+				<a href="http://www.mof.go.kr/" target="_blank">www.mof.go.kr</a>
+			</div>
+			<div class="partnet-item">
+				<img src="<?=$imageurl?>img-partner12.png"/>
+				<p><i class="fa fa-phone"></i> 044.415.1000</p>
+				<a href="http://www.saemangeum.go.kr" target="_blank">www.saemangeum.go.kr</a>
+			</div>
+			<div class="partnet-item">
+				<img src="<?=$imageurl?>img-partner11.png"/>
+				<p><i class="fa fa-phone"></i> 063.280.2114</p>
+				<a href="http://www.jeonbuk.go.kr" target="_blank">www.jeonbuk.go.kr</a>
+			</div>
+			<div class="partnet-item">
+				<img src="<?=$imageurl?>img-partner18.png"/>
+				<p><i class="fa fa-phone"></i> 063.454.4000</p>
+				<a href="http://www.gunsan.go.kr/" target="_blank">www.gunsan.go.kr</a>
+			</div>
 		</div>
 	</div>
 	<!-- subcontent end -->
